c63b56e4da46822c5330e8fcf2d580bd784f373d
Deepak <deepak.bibyan51098@gmail.com>
2020-03-25 17:25:28 +0530

    library imports

--- Cryptocurreny/cryptocurrency.py
+++ Cryptocurreny/cryptocurrency.py
@@ -1,7 +1,10 @@
 import datetime
 import hashlib
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import requests
+from uuid import uuid4
+from urlib.parse import urlparse
 
 class Blockchain:
     
@@ -25,7 +28,7 @@ class Blockchain:
         check_proof = False
         while check_proof is False:
             hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_operation[:4] = '0000':
+            if hash_operation[:4] == '0000':
                 check_proof = True
             else:
                 new_proof += 1
@@ -50,3 +53,39 @@ class Blockchain:
     		previous_block = block
     		block_index +=1
     	return True
+
+
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+	previous_block = blockchain.get_previous_block()
+	previous_proof = previous_block['proof']
+	proof = blockchain.proof_of_work(previous_proof)
+	previous_hash = blockchain.hash(previous_block)
+	block = blockchain.create_block(proof, previous_hash)
+	response = {'message': 'Congratulations, you just mined a block!',
+				'index': block['index'],
+				'timestamp': block['timestamp'],
+				'proof': block['proof'],
+				'previous_hash': block['previous_hash']}
+	return jsonify(response, 200)
+
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+	response = {'chain': blockchain.chain, 
+				'length': len(blockchain.chain)}
+	return jsonify(response), 200
+
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+    return jsonify(response), 200
+
+app.run(host = '0.0.0.0', port = 5000)
